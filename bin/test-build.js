dd88c6eec2f566b5d37a1a3f232430a1a75d3ed1
Martin Sheeks <martin@traction.codes>
2022-10-09 14:35:55 -0500

    add test build command

--- bin/test-build.js
+++ bin/test-build.js
@@ -2,6 +2,7 @@ const { MCPackBuilder } = require('./src/builder')
 require('dotenv').config({ path: `${__dirname}/.env` })
 
 const builder = new MCPackBuilder('', {
+    sourceDir: `../pack`,
     outputDir: process.env.TEST_INSTANCE
 })
 builder.compile()
